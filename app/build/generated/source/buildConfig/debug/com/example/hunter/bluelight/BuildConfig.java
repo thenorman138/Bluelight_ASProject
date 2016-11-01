623a3d2a8598aa10e6b907524aae5bbe5bbda4e4
Ellie <ellie-peterson@mocs.utc.edu>
2016-11-01 13:05:27 -0400

    adding gitignore file

--- app/build/generated/source/buildConfig/debug/com/example/hunter/bluelight/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/hunter/bluelight/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.hunter.bluelight;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.hunter.bluelight";
+  public static final String APPLICATION_ID = "edu.utc.bluelight";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
