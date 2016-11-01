77a6c43d194b39d4c8002ff96ad3b68cbea8ee31
Ellie <ellie-peterson@mocs.utc.edu>
2016-11-01 12:38:42 -0400

    added files to repo. trying to fix the git.

---
+++ app/src/main/java/com/example/hunter/bluelight/choiceActivity.java
@@ -0,0 +1,62 @@
+package com.example.hunter.bluelight;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class choiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+
+        //attributes for the choice to walk with a friend
+        ImageButton Walkbtn = (ImageButton) findViewById(R.id.WWFbtn);
+
+        Walkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(choiceActivity.this, walkActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //attributes for choice to get a safe ride
+        ImageButton rideBtn = (ImageButton) findViewById(R.id.SafeRidebtn);
+
+        rideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(choiceActivity.this, rideActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //attributes for adding a friend
+        ImageButton addBtn = (ImageButton) findViewById(R.id.addFriendbtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(choiceActivity.this, addFriend.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //attributes for dropping a pin or sharing a location
+        ImageButton dropBtn = (ImageButton) findViewById(R.id.dropSharebtn);
+
+        Walkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(choiceActivity.this, walkActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
